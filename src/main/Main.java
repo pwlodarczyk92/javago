ebecb1e0c683b2441837e7b11a030265efb4221a
Pawel Wlodarczyk-Pruszynski <pawel@bielitzlabs.com>
2016-02-21 01:05:21 +0100

    Added mock main and test classes so project can be built.

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by maxus on 20.02.16.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ src/test/Null/NullTest.java
@@ -0,0 +1,14 @@
+package Null;
+
+import org.junit.Test;
+
+/**
+ * Created by maxus on 20.02.16.
+ */
+public class NullTest {
+	@Test
+	public void nullTest() {
+		System.out.println("Hello test!");
+		assert true;
+	}
+}
