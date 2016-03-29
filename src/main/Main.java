a9dae959b89cd0ddb240a5055837d447dc4f4306
Pawel Wlodarczyk-Pruszynski <pawel@bielitzlabs.com>
2016-03-29 21:01:27 +0200

    cleaned up server startup

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,36 +1,11 @@
-import server.GoAPI;
-
-import static spark.Spark.*;
-
 /**
- * Created by maxus on 20.02.16.
+ * Created by maxus on 29.03.16.
  */
 public class Main {
 
-	private GoAPI api = new GoAPI();
-
 	public static void main(String[] args) {
-		Main main = new Main();
-		main.run();
-	}
-
-	private static void enableCORS(final String origin, final String methods, final String headers) {
-		before((request, response) -> {
-			response.header("Access-Control-Allow-Origin", origin);
-			response.header("Access-Control-Request-Method", methods);
-			response.header("Access-Control-Allow-Headers", headers);
-		});
-	}
-
-	public void run() {
-
-		port(8123);
-		enableCORS("*", "*", "*");
-		get("/game", api::get);
-		put("/game", api::put);
-		post("/game", api::post);
-		delete("/game", api::delete);
-
+		Server s = Server.getServer();
+		s.run();
 	}
 
 }
---
+++ src/main/Server.java
@@ -0,0 +1,58 @@
+import server.GoAPI;
+
+import static spark.Spark.*;
+
+/**
+ * Created by maxus on 20.02.16.
+ */
+public class Server {
+
+	//--singleton--
+	private static Server server = null;
+	public static Server getServer() {
+		if (server == null) {
+			server = new Server();
+		}
+		return server;
+	}
+	private Server() {
+	}
+	//--singleton
+
+	public static class Config {
+		public int port = 8123;
+		public boolean cors = false;
+		public String path = "/game";
+	}
+
+	private GoAPI api = new GoAPI();
+	private Config config = new Config();
+	private boolean running = false;
+
+	public void configure(Server.Config config) {
+		if (running)
+			throw new RuntimeException();
+		this.config = config;
+	}
+
+	private static void enableCORS(final String origin, final String methods, final String headers) {
+		before((request, response) -> {
+			response.header("Access-Control-Allow-Origin", origin);
+			response.header("Access-Control-Request-Method", methods);
+			response.header("Access-Control-Allow-Headers", headers);
+		});
+	}
+
+	public void run() {
+
+		running = true;
+		port(config.port);
+		if (!config.cors) enableCORS("*", "*", "*");
+		get(config.path, api::get);
+		put(config.path, api::put);
+		post(config.path, api::post);
+		delete(config.path, api::delete);
+
+	}
+
+}
