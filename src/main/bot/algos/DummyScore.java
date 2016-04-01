3a848845a6e3a0923e972e5b80f8f01e50812d8f
Pawel Wlodarczyk-Pruszynski <pawel@bielitzlabs.com>
2016-04-01 05:14:16 +0200

    added first dummy scoring of possible moves

---
+++ src/main/bot/algos/DummyScore.java
@@ -0,0 +1,61 @@
+package bot.algos;
+
+import core.board.Board;
+import core.primitives.MoveNotAllowed;
+import core.primitives.Stone;
+
+import java.util.HashMap;
+
+/**
+ * Created by maxus on 30.03.16.
+ */
+public class DummyScore {
+
+	private static final Double covconst = 0.3;
+
+	public static <F> HashMap<F, Double> get(Board<F, ?> board) {
+
+		Double maxabs = 0.0;
+		Double basescore = score(board);
+
+		HashMap<F, Double> result = new HashMap<>();
+
+		for (F field: board.tableview().getfields()) {
+			try {
+				Double val = trymove(board, field) - basescore;
+				maxabs = Math.max(maxabs, Math.abs(val));
+				result.put(field, val);
+			} catch (MoveNotAllowed e) { continue; }
+		}
+
+		if(maxabs != 0) {
+			for (F field : result.keySet()) {
+				result.put(field, result.get(field) / maxabs);
+			}
+		}
+
+		return result;
+
+	}
+
+	private static <F> Double trymove(Board<F, ?> board, F field) {
+
+		board.put(field);
+		Double result = score(board);
+		board.undo();
+		return result;
+
+	}
+
+	private static <F> Double score(Board<F, ?> board) {
+
+		HashMap<F, Double> control = Control.get(board.tableview());
+		Double contrresult = 0.0;
+		for(Double i: control.values())
+			contrresult += i;
+		Integer pointresult = board.points(Stone.WHITE) - board.points(Stone.BLACK);
+
+		return contrresult*covconst + pointresult;
+
+	}
+}
